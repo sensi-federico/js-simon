55f20690dde517d95df237500b8ebb073f847ab2
sensiz <dev.federicosensi@gmail.com>
2022-10-19 18:02:40 +0200

    function for prompt

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,53 @@
 // Descrizione:
-// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, 
-// uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). 
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 
 
-// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. 
+// 1- Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. 
 
 // creo una funzione che assegno al bottone
 // genero i numeri e li stampo tramite un for
 const buttonEl = document.querySelector('.btn');
+const buttonReload = document.querySelector('.reload');
+const containerEl = document.querySelector('.container');
 
-buttonEl.addEventListener('click', function(){
+buttonEl.addEventListener('click', function () {
+    buttonEl.style.display = 'none';
     numbers();
+    setTimeout(timer, 1000);
+})
+
+buttonReload.addEventListener('click', function(){
+    location.reload();
 })
 
 function generateRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function numbers () {
+function numbers() {
     for (let i = 0; i < 5; i++) {
         const number = generateRandomNumber(0, 99);
         let numberEl = document.querySelector(`.number-${i}`);
         numberEl.innerHTML = '';
-        numberEl.append(number); 
+        numberEl.append(number);
     }
 }
+
+// 2- Dopo 30 secondi i numeri scompaiono e l'utente deve inserire uno alla volta,
+// i numeri che ha visto precedentemente, tramite il prompt(). 
+
+function timer() {
+    containerEl.style.display = 'none';
+    buttonReload.style.display = 'block';
+}
+
+function userInteract () {
+    const numbList = [];
+    for (let i = 0; i < 5; i++){
+        const userNumber = Number(prompt('Inserisci un numero!'));
+        numbList.push(userNumber);
+    } 
+
+}
+
